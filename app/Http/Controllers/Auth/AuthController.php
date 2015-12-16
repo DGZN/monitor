b7704271e415a47f7a4b591dabe279d2bcd48285
Keiichi Lindley <digizn@gmail.com>
2015-12-16 11:15:45 -0700

    Implement auth middleware on admin pages

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    protected $redirectPath = '/deliveries';
+    protected $redirectPath = '/admin/deliveries';
 
     /**
      * Create a new authentication controller instance.
